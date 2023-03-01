88f401351bf7c09d5e33942a1f3f0340a813a24d
kariman899 <kariman.ramadan2017@gmail.com>
2023-03-01 03:22:35 +0200

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,17 +11,14 @@ export default function Hero({
             sm:h-[55vh] w-auto absolute top-0 left-0 right-0 z-10 opacity-100"></div>
         <div className=" z-20 nike-container grid items-center justify-items-center">
           <div className=" pt-20 md:pt-16 grid justify-items-center ">
-            <h1 className=" text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold  drop-shadow-sm text-slate-200">{title}</h1>
+            <h1 className=" text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl  text-center font-extrabold  drop-shadow-sm text-slate-200">{title}</h1>
             <h3 className=" pt-2 text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold  drop-shadow-sm text-slate-200">{subtitle}</h3>
             <button type="button" className=" button-theme  bg-slate-200 mt-5 mb-8 py-2 px-6">{btntext}</button>
           </div>
-          {/* <div className="">
-
-          </div> */}
          {/**/}
           <div className="">
             <img src={img} alt="hero img" className=" w-auto h-[45vh]  
-            md:h-[32vh]  xsm:h-[18vh] transitions-theme rotate-[-25deg] hover:rotate-0  " />
+            md:h-[32vh] sm:h-[25vh] xsm:h-[24vh] transitions-theme rotate-[-25deg] hover:rotate-0  " />
           </div>
         </div>
       </div>
