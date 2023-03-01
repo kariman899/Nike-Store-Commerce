0913b852a39303bafd71be4c037b9e39044f0558
kariman899 <kariman.ramadan2017@gmail.com>
2023-03-02 00:12:58 +0200

    update cart

--- src/components/Cart/TotalCart.jsx
+++ src/components/Cart/TotalCart.jsx
@@ -5,7 +5,7 @@ export default function TotalCart({clearCart,cartAmout}) {
   return (
     <>
     <div className=" fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
-            <div className=" flex justify-between items-center">
+            <div className="my-2 flex justify-between items-center">
               <h5 className=" font-medium">SubTotal</h5>
               <span
                 className="bg-theme-cart text-slate-100 px-1 rounded-sm shadow 
@@ -14,17 +14,10 @@ export default function TotalCart({clearCart,cartAmout}) {
                 ${cartAmout}
               </span>
             </div>
-            <div className=" text-center">
-              <p className="my-1 text-sm ">
-                Taxes and Shipping Will Calculate At Shipping
-              </p>
-              <div className=" flex justify-between gap-2">
-                <button onClick={clearCart} className="bg-theme-cart text-slate-100 px-1 rounded-sm shadow shadow-slate-900 w-3/5 py-1">
+            <div className="mt-2 text-center">
+              <div className="flex justify-between gap-2">
+                <button onClick={clearCart} className="bg-theme-cart text-slate-100 px-1 rounded-sm shadow shadow-slate-900 w-full py-1">
                   Clear Cart
-                </button>
-
-                <button className="bg-theme-cart text-slate-100 px-1 rounded-sm shadow shadow-slate-900 w-3/5 py-1">
-                  Check Out
                 </button>
               </div>
             </div>
