f0c92ba76c95f4c98d61fee6b114f075f3e89791
kariman899 <kariman.ramadan2017@gmail.com>
2023-03-03 23:31:15 +0200

    update cart

--- src/components/Cart/CountCart.jsx
+++ src/components/Cart/CountCart.jsx
@@ -4,9 +4,9 @@ export default function CountCart({onCartToggle,totalQTY}) {
   return (
     <>
      <div className=' flex items-center justify-between p-2 bg-white '>
-        <div className=' flex gap-2'>
+        <div className=' flex items-center gap-2 sm:gap-1'>
             <div className=' cursor-pointer grid items-center ' onClick={onCartToggle}>
-                <ChevronDoubleLeftIcon className=' w-5 h-5 text-slate-900  hover:text-orange-500'/>
+                <ChevronDoubleLeftIcon className=' w-5 h-5 sm:w-4 sm:h-4 text-slate-900  hover:text-orange-500'/>
             </div>
             <div className=''>
                 <h4 className=' text-sm font-medium text-slate-900'>Your Cart <span className=' font-normal bg-theme-cart text-slate-100 px-2 py-0.5 rounded-sm shadow shadow-slate-900'>
@@ -14,7 +14,7 @@ export default function CountCart({onCartToggle,totalQTY}) {
             </div>
         </div>
         <div className='grid items-center ' onClick={onCartToggle}>
-            <button className='bg-theme-cart text-slate-100 p-0.5 rounded-sm shadow shadow-slate-900 active:scale-90'> <XMarkIcon className=' w-5 h-5'></XMarkIcon> </button>
+            <button className='bg-theme-cart text-slate-100 p-0.5 rounded-sm shadow shadow-slate-900 active:scale-90'> <XMarkIcon className=' w-5 h-5 sm:w-4 sm:h-4'></XMarkIcon> </button>
         </div>
      </div>   
     </>
