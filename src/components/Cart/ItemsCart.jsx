3440765913d378c2522050886c551314df46b410
kariman899 <kariman.ramadan2017@gmail.com>
2023-03-03 23:00:45 +0200

    update itemCart

--- src/components/Cart/ItemsCart.jsx
+++ src/components/Cart/ItemsCart.jsx
@@ -31,29 +31,30 @@ export default function ItemsCart({
   }
   return (
     <>
-      <div className="flex justify-between p-4">
-        <div className="flex gap-5 items-center">
+    <div className="flex justify-between p-4 xsm:px-1.5 xsm:py-2  items-center">
+        <div className="flex gap-5 xsm:gap-2  items-center ">
           <div
             className={` bg-gradient-to-b ${color} ${shadow} p-3 rounded relative 
           transition-all duration-200 ease-in-out hover:scale-105`}
           >
-            <img src={img} className="w-32 lg:w-28 " alt="" />
-            <div className='absolute right-1 top-1 blur-theme-effect bg-white/80 text-black text-xs px-1 rounded'>${price}</div>
+            <img src={img} className="w-32 lg:w-28 xsm:w-16 " alt="" />
+            <div className='absolute right-1 top-1 blur-theme-effect bg-white/80 text-black text-xs xsm:text-[10px] px-1 rounded '>
+              ${price}</div>
           </div>
           
           <div>
             <div>
-              <h4 className=" font-medium text-lg text-slate-900 lg:text-sm">
+              <h4 className=" font-medium text-lg text-slate-900 lg:text-sm xsm:text-xs">
                 {title}
               </h4>
-              <p className="text-sm text-slate-700 lg:text-xs">{text}</p>
+              <p className="text-sm text-slate-700 lg:text-xs xsm:text-[10px]">{text}</p>
             </div>
-            <div className=" flex items-center gap-4 mt-2">
+            <div className=" flex items-center gap-4 xsm:gap-2 mt-2">
               <button className="bg-theme-cart text-slate-100 p-0.5 rounded shadow shadow-slate-900 active:scale-90"
               onClick={onDecreaseQTY}>
                 <MinusIcon className="w-5 h-5 lg:w-4 lg:h-4" />{" "}
               </button>
-              <span>{cartQuantity}</span>
+              <span className="xsm:text-sm">{cartQuantity}</span>
               <button className="bg-theme-cart text-slate-100 p-0.5 rounded-sm shadow shadow-slate-900 active:scale-90"
               onClick={onIncreaseQTY}>
                 <PlusIcon className="w-5 h-5 lg:w-4 lg:h-4" />{" "}
@@ -61,9 +62,9 @@ export default function ItemsCart({
             </div>
           </div>
         </div>
-        <div className=" grid  ">
+        <div className=" grid justify-between xsm:gap-5 ">
           <div>
-            <span className=" text-slate-900 ">${price * cartQuantity}</span>
+            <span className=" text-slate-900 xsm:text-sm ">${price * cartQuantity}</span>
           </div>
           <div className=" grid items-center justify-center ">
             <button
